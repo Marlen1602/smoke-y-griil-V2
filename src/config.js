41b3f348b54ab381e14e81050dc416341afa3582
Jesus Rosales Castillo <jesusrosales07537@gmail.com>
2024-11-06 20:40:44 -0600

    cambio mongo_uri

--- src/config.js
+++ src/config.js
@@ -1,8 +1,7 @@
 export const TOKEN_SECRET='some secret key'
 
 
-export const MONGODB_URI = "mongodb+srv://jesusrctest:sOxYwRNkuZg4qa8k@cluster0.mz7xg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"; 
 
-// export const MONGODB_URI = "mongodb+srv://marlenh652:Marlen1602@smokeygrill.krrv9.mongodb.net/?retryWrites=true&w=majority&appName=smokeygrill"; 
+export const MONGODB_URI = "mongodb+srv://marlenh652:Marlen1602@smokeygrill.krrv9.mongodb.net/?retryWrites=true&w=majority&appName=smokeygrill"; 
 
 // export const MONGODB_URI = "mongodb+srv://marlenh652:Marlen1602@smokeygrill.krrv9.mongodb.net/smokeygrill"; 
