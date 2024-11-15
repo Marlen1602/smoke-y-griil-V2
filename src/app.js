e9f83c42d19deaa447dbb9902a9bd5d162370c35
Marlen <marlen04h@gmail.com>
2024-11-14 23:01:16 -0600

    link

--- src/app.js
+++ src/app.js
@@ -13,7 +13,8 @@ app.use(cookieParser());
 app.use(xss());
 
 app.use(cors({
-    origin:'https://smokeygrill.vercel.app',
+    origin: ['https://smokeygrill.vercel.app', 'http://localhost:5173'], // Agrega la URL de tu frontend en producción y en desarrollo local si es necesario
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
     //origin: 'http://localhost:5173',
     credentials: true
 }));
