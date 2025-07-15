1a0d671e6669deeb2180edeb9acec52a9099a650
Marlen1602 <marlen04h@gmail.com>
2025-07-15 12:59:43 -0600

    cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -48,7 +48,7 @@ app.use(cookieParser());
 
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "*",
     //https://slategray-butterfly-754330.hostingersite.com
     credentials: true, // Permitir cookies
 }));
